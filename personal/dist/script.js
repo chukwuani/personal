0e4bb9b3dcf561485429f7d4a3d18f6934e7f402
chukwuani <107764507+chukwuani@users.noreply.github.com>
2022-08-18 15:23:04 +0000

    Update script.js

--- personal/dist/script.js
+++ personal/dist/script.js
@@ -1,25 +1,64 @@
-const sections = document.querySelectorAll('section')
+var loader = document.querySelector(".loader")
+var main = document.querySelector("main")
 
-window.addEventListener('scroll', checkSections)
+window.addEventListener("load", vanish)
 
-checkSections()
 
-function checkSections(){
-    const triggerBottom = window.innerHeight / 5 * 4
+function vanish(){
+    setTimeout(() =>{
+        loader.classList.add("vanish");
 
-    sections.forEach(section => {
-        const sectionTop = section.getBoundingClientRect().top
 
-        if(sectionTop < triggerBottom){
-            section.classList.add('show')
-        } else{
-            section.classList.remove('show')
-        }
-    })
+        main.style.display = 'block';
+        setTimeout(() =>main.style.opacity = 1, 50);
+        
+
+    }, 3000);
+    
 }
 
-document.getElementsByClassName(".copy-text").onclick = function() {
-    this.select();
-    document.execCommand('copy');
-    alert(window.getSelection().toString());
+//Get the button
+var mybutton = document.querySelector(".scroll-down");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 3500 || document.documentElement.scrollTop > 3500) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
   }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// const boxes = document.querySelectorAll('.noshow')
+
+// window.addEventListener("scroll", checkBoxes)
+// checkBoxes()
+
+// function checkBoxes(){
+//   const triggerBottom =  window.innerHeight / 10 
+
+//   boxes.forEach(leave => {
+//     const boxTop = leave.getBoundingClientRect().top
+
+//     if(boxTop < triggerBottom){
+//         leave.classList.add('show')
+//     }
+//     else{
+//         leave.classList.remove('show') 
+//     }
+//   })
+// }
+
+// document.getElementsByClassName(".copy-text").onclick = function() {
+//     this.select();
+//     document.execCommand('copy');
+//     alert(window.getSelection().toString());
+//   }
